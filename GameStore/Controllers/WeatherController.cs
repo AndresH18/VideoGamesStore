5f74eff2e740aa7ea80c95210b96339216069ed6
Andres_a <andres.hoyos18@eia.edu.co>
2023-02-16 12:19:14 -0500

    Added Swagger and Api middlewares

---
+++ GameStore/Controllers/WeatherController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hola");
+    }
+}
