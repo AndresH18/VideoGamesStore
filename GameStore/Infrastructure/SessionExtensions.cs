8e5b15bfe531dd7a2f4dc30f18e5c4cfd9b7a8f9
Andres Hoyos <andres.hoyos18@eia.edu.co>
2023-02-18 20:52:01 -0500

    Added method extensions for Session storage and getting route string

---
+++ GameStore/Infrastructure/SessionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+
+namespace GameStore.Infrastructure;
+
+/// <summary>
+/// /// Static methods to interact with <b>Session</b> storage.
+/// </summary>
+public static class SessionExtensions
+{
+    /// <summary>
+    /// Stores the <paramref name="value"/> on the Session data using the <paramref name="key"/>
+    /// </summary>
+    /// <param name="session"></param>
+    /// <param name="key">The key to retrieve the <paramref name="value"/></param>
+    /// <param name="value">The <see cref="Object"/> to store in the Session storage</param>
+    public static void SetJson(this ISession session, string key, object value)
+    {
+        session.SetString(key, JsonSerializer.Serialize(value));
+    }
+
+    /// <summary>
+    /// Retrieves <typeparamref name="T"/> from the Session storage
+    /// </summary>
+    /// <param name="session"></param>
+    /// <param name="key">The key of the object</param>
+    /// <typeparam name="T">The type to deserialize the object </typeparam>
+    /// <returns>The deserialized object or null.</returns>
+    /// <exception cref="JsonException">The JSON is invalid. - or - <typeparamref name="T"/> is not compatible with the JSON. - or - There is remaining data in the string beyond a single JSON value.</exception>
+    /// <exception cref="NotSupportedException">There is no compatible <see cref="System.Text.Json.Serialization.JsonConverter"/> for <typeparamref name="T"/> or its serializable members.</exception>
+    public static T? GetObject<T>(this ISession session, string key)
+    {
+        var data = session.GetString(key);
+        return data != null ? JsonSerializer.Deserialize<T>(data) : default;
+    }
+}
---
+++ GameStore/Infrastructure/UrlExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace GameStore.Infrastructure;
+
+public static class UrlExtensions
+{
+    /// <summary>
+    /// Return the Path and query string of the <see cref="HttpRequest"/>
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    public static string PathAndQuery(this HttpRequest request) =>
+        request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.Path.ToString();
+}
